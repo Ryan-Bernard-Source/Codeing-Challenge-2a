dfa289df904f937963760ce5e3987ac20ea9a301
Ryan Bernard <rbernard143@usf.edu>
2025-09-06 18:26:46 -0400

    Updated Code

--- cc_1.js
+++ cc_1.js
@@ -5,14 +5,18 @@ let costPerUnit = 27.99;
 let basePrice = 45.99;
 let discountRate = 0.50;
 let salesTaxRate = 0.05;
-fixedMonthlyCosts = 
+fixedMonthlyCosts = 500;
 // Formulas for Price and Profit
-
+let discountedPrice = basePrice * (1-discountRate);
+let finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit;
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+let isProfitablePerUnit = profitPerUnit > 0;
 //Print to Console
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
+console.log("Product Name:" + productname);
+console.log("Cost per Unit: $" + costPerUnit)
+console.log("Final Price with Tax: $" + finalPriceWithTax)
+console.log("Profit per Unit: $" + profitPerUnit)
+console.log("Break-even Units: " + breakEvenUnits)
+console.log("Per Unit profitability:" + isProfitablePerUnit)
 // Code goes here
