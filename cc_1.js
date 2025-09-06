e48f245d3517542b265e456ed1269ab94f004988
Ryan Bernard <rbernard143@usf.edu>
2025-09-06 18:14:23 -0400

    Forgot to add all console logs to display output.

---
+++ cc_1.js
@@ -0,0 +1,18 @@
+// Coding Challenge 2a
+// Variables Created
+let productname = "Bluetooth Earbuds";
+let costPerUnit = 27.99;
+let basePrice = 45.99;
+let discountRate = 0.50;
+let salesTaxRate = 0.05;
+fixedMonthlyCosts = 
+// Formulas for Price and Profit
+
+//Print to Console
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+// Code goes here
